fc518f117301d39c40634c11992c4516111d0059
Tharusha Kodithuwakku <tharusha.20232275@iit.ac.lk>
2025-03-05 14:19:25 +0530

    Create test-1.js - testings

---
+++ test-1.js
@@ -0,0 +1,17 @@
+let count = 0;
+
+function increase() {
+    count++;
+    console.log("Count:", count);
+}
+
+function decrease() {
+    count--;
+    console.log("Count:", count);
+}
+
+// Example usage
+increase(); // Count: 1
+increase(); // Count: 2
+decrease(); // Count: 1
+
